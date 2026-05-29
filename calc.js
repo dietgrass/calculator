3284aeb4d430b812615672a7f9b7e6be40d66563
dietgrass <yoissel1@gmail.com>
2026-05-29 00:14:24 -0400

    first commit

---
+++ calc.js
@@ -0,0 +1,23 @@
+let buttons = document.querySelectorAll("button");
+
+let computable = document.querySelector("#inputDiv");
+
+computable.textContent = "";
+
+buttons.forEach(function(button){
+    
+    button.addEventListener("click", function(){
+        
+        if (button.className !== "result" && button.className !== "clear"){
+            computable.textContent += button.innerText;
+        }
+        else if (button.className === "result"){
+            computable.textContent = eval(computable.textContent);
+            
+        }
+        else{
+            computable.textContent = ""
+        }
+        
+    })
+})
